5a28460d54243257d1bbfb01d8037a6279ded24d
Prakash, Deva <devaprakash619@gmail.com>
2025-07-18 08:51:37 +0530

    deployment

--- src/main/java/com/lurniq/LurniqApiApplication.java
+++ src/main/java/com/lurniq/LurniqApiApplication.java
@@ -2,15 +2,8 @@ package com.lurniq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    SystemMetricsAutoConfiguration.class,
-    MetricsAutoConfiguration.class,
-    TomcatMetricsAutoConfiguration.class
-})
+@SpringBootApplication()
 public class LurniqApiApplication {
 
 	public static void main(String[] args) {
