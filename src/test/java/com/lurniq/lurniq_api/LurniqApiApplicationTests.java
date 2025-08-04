a170728acf49838221bfd7e40586a21204770fb7
Prakash, Deva <devaprakash619@gmail.com>
2025-08-04 19:08:24 +0530

    Native Image

--- src/test/java/com/lurniq/lurniq_api/LurniqApiApplicationTests.java
+++ src/test/java/com/lurniq/lurniq_api/LurniqApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.lurniq.lurniq_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LurniqApiApplicationTests {
 
 	@Test
